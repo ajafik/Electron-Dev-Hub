219c7c7abf2358d4febda5d67c02a5236ad2a19d
ajafik <oluwafikayoajayi@gmail.com>
2020-04-29 23:51:47 +0100

    music player app initialized

---
+++ music-player/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+let window;
+
+function createWindow() {
+    window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    window.loadFile('index.html');
+}
+
+app.on("ready", createWindow);
