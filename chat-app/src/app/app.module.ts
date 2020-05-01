7b45e453a4b7f2c61fccd102fa0b98713edbf5e0
ajafik <oluwafikayoajayi@gmail.com>
2020-05-01 01:15:03 +0100

    firebase authentication implemenatation

--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -4,16 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule } from '@angular/material/button';
+import {MatInputModule } from '@angular/material/input';
+
+
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ChatComponent } from './chat/chat.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChatComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
